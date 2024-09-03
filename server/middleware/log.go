8b2ece2975131812481b063b51c7fa570ad299c3
blkcor <blkcor.dev@gmail.com>
2024-09-03 10:56:56 +0800

    chore: 日志添加接口响应状态

--- server/middleware/log.go
+++ server/middleware/log.go
@@ -10,9 +10,16 @@ import (
 func RequestLogMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Log requests details
-		info := fmt.Sprintf("%s | %s | %s %s", c.Request.Method, c.Request.URL.Path, c.ClientIP(), c.Request.UserAgent())
-		logger.Info(info)
+		reqInfo := fmt.Sprintf("%s | %s | %s %s", c.Request.Method, c.Request.URL.Path, c.ClientIP(), c.Request.UserAgent())
 		// Process requests
 		c.Next()
+		//响应信息
+		respInfo := fmt.Sprintf("[%d]", c.Writer.Status())
+		info := fmt.Sprintf("%s -> %s", reqInfo, respInfo)
+		if c.Writer.Status() == 200 {
+			logger.Info(info)
+		} else {
+			logger.Error(info)
+		}
 	}
 }
